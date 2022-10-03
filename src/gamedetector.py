e5b57f87b165ecf537863b637b08a79d6a44ea1f
cetteup <me@cetteup.com>
2022-10-03 22:21:05 +0200

    feat: Add support for Battlefield 2042

--- src/gamedetector.py
+++ src/gamedetector.py
@@ -11,6 +11,7 @@ class GameDetector:
         'Battlefield 1': '^battlefield™ 1$',
         'Battlefield 1942': r'^bf1942 \(ver: \w{3}, \d+ \w{3} \d+ [:0-9]+\)$',
         'Battlefield 2': r'^bf2 \(v1.[\.\-0-9]+, pid: [0-9]+\)$',
+        'Battlefield 2042': '^battlefield™ 2042$',
         'Battlefield 2142': r'^bf2142 \(v1.[\.\-0-9]+, pid: [0-9]+\)$',
         'Battlefield 3': '^battlefield 3™$',
         'Battlefield 4': '^battlefield 4$',
