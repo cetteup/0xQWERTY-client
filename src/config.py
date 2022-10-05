cf4f879bd15b2b1c182f5c61d2cdc6d4cc9a3189
cetteup <me@cetteup.com>
2022-10-06 00:13:17 +0200

    chore: Remove obsolete dashboard uri from config.py

--- src/config.py
+++ src/config.py
@@ -7,7 +7,6 @@ LISTEN_ADDR = '127.0.0.1'
 LISTEN_PORT = 8000
 BASE_URL = f'http://localhost:{LISTEN_PORT}'
 REDIRECT_URI = f'{BASE_URL}/s/auth-callback'
-DASHBOARD_URI = f'{BASE_URL}/s/dashboard'
 TWITCH_AUTH_BASE_URL = 'https://id.twitch.tv/oauth2/authorize'
 CLIENT_ID = 'jzaeeic6j23u0l2onzm2orovs0uakl'
 SCOPES = ['channel:read:redemptions', 'channel:manage:redemptions']
