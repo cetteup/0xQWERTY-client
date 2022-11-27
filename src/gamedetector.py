dce03a39186f190982af507b330745f4e4062fee
cetteup <me@cetteup.com>
2022-11-27 15:26:56 +0100

    feat: Add support for Call of Duty United Offensive

--- src/gamedetector.py
+++ src/gamedetector.py
@@ -23,6 +23,7 @@ class GameDetector:
         'Call of Duty 4': '^call of duty 4$',
         'Call of Duty Modern Warfare': '^call of duty®: modern warfare®$',
         'Call of Duty Modern Warfare II': '^call of duty® hq$',
+        'Call of Duty United Offensive': '^cod:united offensive (?:single|multi)player$',
         'Counter-Strike': '^counter-strike$',
         'Counter-Strike Condition Zero': '^condition zero$',
         'Counter-Strike Global Offensive': '^counter-strike: global offensive$',
