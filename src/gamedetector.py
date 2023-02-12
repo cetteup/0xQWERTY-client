d9918c1def8ceeebb8dceebce7047f2cddecd622
cetteup <me@cetteup.com>
2023-02-12 19:19:21 +0100

    feat: Add support for Unreal Tournament 2004

--- src/gamedetector.py
+++ src/gamedetector.py
@@ -62,6 +62,7 @@ class GameDetector:
         'The Sims 4': '^the sims™ 4$',
         'Trackmania': '^trackmania$',
         'Unreal Tournament': '^unreal tournament$',
+        'Unreal Tournament 2004': '^unreal tournament 2004$',
         'Valorant': '^valorant$',
         'Warhammer Vermintide 2': '^warhammer: vermintide 2$',
         'War Thunder': '^war thunder(?: - in battle)?$',
