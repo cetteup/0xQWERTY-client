d7adaf225cf5045e9d07b6c46d4964f02af7b30e
cetteup <me@cetteup.com>
2023-03-04 09:43:12 +0100

    fix: Remove extra parentheses in BF1942 detection regex

--- src/gamedetector.py
+++ src/gamedetector.py
@@ -10,7 +10,7 @@ class GameDetector:
         'Age of Empires II Definitive Edition': '^age of empires ii: definitive edition$',
         'Apex Legends': '^apex legends$',
         'Battlefield 1': '^battlefield™ 1$',
-        'Battlefield 1942': r'^bf1942 \(ver: \w+, \d+ \w+\.? \d+(?: [:0-9]+)?\)))$',
+        'Battlefield 1942': r'^bf1942 \(ver: \w+, \d+ \w+\.? \d+(?: [:0-9]+)?\)$',
         'Battlefield 2': r'^bf2 \(v1.[\.\-0-9]+, pid: [0-9]+\)$',
         'Battlefield 2042': '^battlefield™ 2042$',
         'Battlefield 2142': r'^bf2142 \(v1.[\.\-0-9]+, pid: [0-9]+\)$',
